3db842ee6d9abb66c7117b5a32eaae4fdc9ebc3b
frank <ochoa.franco132@gmail.com>
2024-08-09 11:05:14 -0300

    creating CLIENT CRUD functionality

--- api/src/main/java/com/kitchenware/api/controller/SellController.java
+++ api/src/main/java/com/kitchenware/api/controller/SellController.java
@@ -1,9 +1,77 @@
 package com.kitchenware.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kitchenware.api.entity.Client;
+import com.kitchenware.api.entity.Product;
+import com.kitchenware.api.service.IClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/sells")
 public class SellController {
+    @Autowired
+    private IClientService clientService;
+
+    // Create a client.
+    @PostMapping("/create")
+    public ResponseEntity<Client> saveClient(@RequestBody Client client) {
+        clientService.saveClient(client);
+        return new ResponseEntity<>(client, HttpStatus.CREATED);
+    }
+
+    // Read all clients.
+    @GetMapping()
+    public ResponseEntity<List<Client>> getClients() {
+        List<Client> clientList = clientService.getClients();
+        return new ResponseEntity<>(clientList, HttpStatus.OK);
+    }
+
+    // Read one client.
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> getClientById(@PathVariable Long id) {
+        Optional<Client> optionalClient = clientService.getClientById(id);
+        if (optionalClient.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        Client client = optionalClient.get();
+
+        return new ResponseEntity<>(client, HttpStatus.OK);
+    }
+
+    // Update a whole client.
+    @PutMapping("/edit")
+    public ResponseEntity<Client> editClient(@RequestBody Client client) {
+        clientService.saveClient(client);
+        return new ResponseEntity<>(client, HttpStatus.OK);
+    }
+
+    // Update client by its ID.
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<Client> editClient(
+            @PathVariable Long id,
+            @RequestBody Client client) {
+
+        Optional<Client> optionalClient = clientService.getClientById(id);
+        if (optionalClient.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        clientService.editClient(id, client);
+        return new ResponseEntity<>(client, HttpStatus.OK);
+    }
+
+    // Delete a client by its ID.
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteClient(@PathVariable Long id) {
+        try {
+            clientService.deleteClient(id);
+            return new ResponseEntity<>("The Client was deleted", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("There was an error", HttpStatus.BAD_REQUEST);
+        }
+    }
 }
